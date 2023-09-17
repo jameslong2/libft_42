99d92bb72210a09d5e8d30297819fcf15f72f491
jameslong2 <jaumecarrion@gmail.com>
2023-09-17 11:03:54 +0200

    subido

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,19 +6,15 @@
 /*   By: jaucarri <jaucarri@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 09:42:07 by jaucarri          #+#    #+#             */
-/*   Updated: 2023/09/16 10:45:13 by jaucarri         ###   ########.fr       */
+/*   Updated: 2023/09/17 11:03:42 by jaucarri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-size_t	ft_memcpy(char *dst, const char *src, size_t dstsize);
-
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
 int		ft_strlen(const char *s);
-
-void	*ft_memset(void *b, int c, size_t len);
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
@@ -27,9 +23,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i[0] = 0;
 	i[1] = 0;
-	i[2] = 0;
+	i[2] = ft_strlen(s1);
 	i[3] = ft_strlen(s1) + ft_strlen(s2);
-	tmp = (char *)malloc(i[3] + 1);
+	tmp = (char *)malloc(i[3] * sizeof(char) + 1);
 	if (tmp == 0)
 		return (0);
 	else if (ft_strlen(s1) == 0 && ft_strlen(s2) > 0)
@@ -38,13 +34,15 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		ft_strlcpy(tmp, (const char *)s1, (size_t)ft_strlen(s1) + 1);
 	else
 	{
-		while (i[0]++ < i[3])
+		while (i[0] < i[3])
 		{
-			if (s1[i[1]] != '\0')
-				tmp[i[1]] = s1[i[1]];
-			tmp[(i[1]++) + ft_strlen(s1)] = s2[i[2]++];
+			if (*s1)
+				tmp[i[1]++] = *((char *)s1++);
+			if (*s2)
+				tmp[i[2]++] = *((char *)s2++);
+			i[0]++;
 		}
-		tmp[i[1] + ft_strlen(s1)] = '\0';
+		tmp[i[0]] = '\0';
 	}
 	return (tmp);
 }
